75dedb24cde540c2ad3fe27cd3090f2fcca22d0e
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-08-28 13:38:07 +0700

    remove somes menu, add version

--- views/MenuScreen/MenuScreen.js
+++ views/MenuScreen/MenuScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { connect } from 'react-redux';
 import { 
@@ -10,14 +10,14 @@ import {
 import { GradientLayout, HeaderLayout } from '../components';
 import { HP, WP } from '../config/layout';
 import PropTypes from 'prop-types';
+import Constants from 'expo-constants';
 
 const menus = [
     { id: '1', title: 'Produksi Kiriman', subtitle: 'Pengawasan Produksi Kiriman', route: 'ProduksiKiriman'},
-    { id: '2', title: 'Transaksi Loket', subtitle: 'Pengawasan Transaksi Loket', route: 'KirimanMenginap'},
     { id: '3', title: 'Kiriman Menginap', subtitle: 'Pengendalian Kiriman Potensi Menginap', route: 'KirimanMenginap' },
     { id: '4', title: 'Kiriman Terbuka', subtitle: 'Pengendalian Kiriman Terbuka', route: 'KirimanMenginap'},
-    { id: '5', title: 'Antaran', subtitle: 'Pengendalian Antaran', route: 'KirimanMenginap'},
-    { id: '6', title: 'Kiriman Jatuh Tempo', subtitle: '', route: 'KirimanMenginap'}
+    { id: '5', title: 'Kiriman Jatuh Tempo', subtitle: '', route: 'KirimanMenginap'},
+    { id: '6', title: 'Kiriman Irregularitas', subtitle: 'Pengendalian kiriman irregularitas', route: 'KirimanMenginap'},
 ]
 
 const MenuScreen = ({ navigation, sessions }) => {
@@ -77,8 +77,11 @@ const MenuScreen = ({ navigation, sessions }) => {
                     keyExtractor={item => item.id}
                     ListHeaderComponent={header}
                     showsVerticalScrollIndicator={false}
+                    ListFooterComponent={<View style={styles.footer}>
+                        <Text style={styles.version}>Current version { Constants.manifest.version }</Text>
+                    </View>}
                 />
-            </View>
+            </View>            
         </GradientLayout>
     )
 }
@@ -139,6 +142,15 @@ const styles = StyleSheet.create({
         color: "#FFF",
         fontFamily: 'Poppins-Regular',
         fontSize: RFValue(11)
+    },
+    footer: {
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    version: {
+        color: '#FFF',
+        textAlign: 'center'
     }
 })
 
